ae20034191db7b46cd08d9eca0eec1f925fcb652
Mohammad Zafari <64021673+m-zafari@users.noreply.github.com>
2025-04-12 02:08:30 +0330

    Add files via upload

---
+++ preFixCalculator.py
@@ -0,0 +1,31 @@
+
+#mohammad zafari
+
+def is_operand(k): 
+    return k.isdigit() 
+def calculated(expression): 
+    stack = [] 
+    for c in expression[::-1]: 
+        if is_operand(c): 
+            stack.append(int(c)) 
+        else: 
+            o1 = stack.pop() 
+            o2 = stack.pop() 
+  
+            if c == '+': 
+                stack.append(o1+ o2 ) 
+  
+            elif c == '-': 
+                stack.append(o1- o2 ) 
+  
+            elif c == '*': 
+                stack.append(o1* o2 ) 
+  
+            elif c == '/': 
+                stack.append(o1 / o2 ) 
+    return stack.pop() 
+    
+if __name__ == "__main__": 
+    test1 = "-+8/632"
+    print(test1)
+    print(calculated(test1)) 
